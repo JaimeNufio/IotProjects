76e19d8c2c40ab0c79fe62e3ad088e83c4fb19e3
Jaime Nufio <jaimeenufio@gmail.com>
2023-08-06 00:53:41 -0400

    recall route added

--- Server/server.js
+++ Server/server.js
@@ -31,7 +31,7 @@ async function recordInteraction(res,name) {
     const values = [name];
     const insertQuery = `
       INSERT INTO timekeep (name,time)
-      VALUES ($1, CURRENT_TIME)
+      VALUES ($1, CURRENT_TIMESTAMP)
       RETURNING *;
     `;
 
@@ -42,12 +42,55 @@ async function recordInteraction(res,name) {
 
   } catch (err) {
     console.error('Error occurred:', err);
-    // res.status(400).send("Error during interaction for device, "+name);
   }
 }
 
+async function recallInteraction(res,name){
+  const client = await pool.connect();
+
+  console.log('connected via recall rt')
+
+  try {
+    values = [name]
+    const query = `
+      SELECT EXTRACT(EPOCH FROM "time" AT TIME ZONE 'UTC') AS unix_timestamp
+      FROM timekeep
+      WHERE NAME = '${values[0]}'
+      ORDER BY id DESC
+      LIMIT 1;
+    `;
+
+    const result = await client.query(query)
+
+
+    if (result.rows.length > 0) {
+      const row = result.rows[0]
+      console.log(row.unix_timestamp)
+      res.send(result.unix_timestamp);
+    } else {
+      console.log(0);
+      res.send(0)
+    }
+  } catch (error) {
+    console.error('Error executing query:', error);
+  } finally {
+    client.release();
+  }
+};
+
+
+
+
+
+// button pressed, record the interaction and return back the time stamp
 app.get('/interact', (req, res) => {
   recordInteraction(res, req.query.name)
+});
+
+// device powered on, fetch last time stored.
+app.get('/recall', (req, res) => {
+
+  recallInteraction(res, req.query.name)
 });
 
 app.listen(port, () => {
